f544d9b56e674204dcf3026d42c293776494f4fc
unknown <1687653101@qq.com>
2026-01-02 11:16:16 +0800

    Initial commit: Spring Boot project setup with all dependencies

---
+++ src/main/java/com/university/foreignstudent/ForeignStudentApplication.java
@@ -0,0 +1,13 @@
+package com.university.foreignstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForeignStudentApplication.class, args);
+    }
+}
+
